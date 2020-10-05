b4dfd90365e7a02f07235fcba848e6696ffc9ff6
Daniel Springwald <Springwald@users.noreply.github.com>
2020-10-05 22:21:56 +0200

    switched to char length instead of pixel length

--- src/de.springwald.xml/editor/helper/TextSplitHelperNew.cs
+++ src/de.springwald.xml/editor/helper/TextSplitHelperNew.cs
@@ -9,12 +9,13 @@ namespace de.springwald.xml.editor.helper
         public class TextPart
         {
             public string Text { get; set; }
-            public Rectangle Rectangle { get; set; }
+            public int LineNo { get; set; }
             public bool Inverted { get; set; }
         }
 
-        public static IEnumerable<TextPart> SplitText(string text, int invertiertStart, int invertiertLaenge, PaintContext paintContext, int lineSpaceY, int fontHeight, float fontWidth)
+        public static IEnumerable<TextPart> SplitText(string text, int invertiertStart, int invertiertLaenge, int maxLength, int maxLengthFirstLine)
         {
+            var lineNo = 0;
             var invertiertEnd = invertiertStart + invertiertLaenge;
             var pos = 0;
             var watchOutPos = 0;
@@ -23,6 +24,7 @@ namespace de.springwald.xml.editor.helper
             var wasInverted = inverted;
             var splitHere = false;
             var startNewLine = false;
+            var maxLengthThisLine = maxLengthFirstLine;
 
             while (watchOutPos < text.Length)
             {
@@ -33,8 +35,11 @@ namespace de.springwald.xml.editor.helper
                 charAtPos = text[pos];
                 if (charAtPos == ' ') // next chance to split
                 {
-                    startNewLine = true;
-                    splitHere = true;
+                    if (watchOutPos - pos >= maxLengthThisLine)
+                    {
+                        startNewLine = true;
+                        splitHere = true;
+                    }
                 }
                 if (inverted)
                 {
@@ -61,6 +66,11 @@ namespace de.springwald.xml.editor.helper
                         Text = text.Substring(pos, watchOutPos - pos)
                     };
                     pos = watchOutPos ;
+                    if (startNewLine)
+                    {
+                        lineNo++;
+                        maxLengthThisLine = maxLength;
+                    }
                 }
             }
         }
--- src/de.springwald.xmlTests/editor/helper/TextSplitHelperNewTests.cs
+++ src/de.springwald.xmlTests/editor/helper/TextSplitHelperNewTests.cs
@@ -10,28 +10,10 @@ namespace de.springwald.xml.editor.helper.Tests
     [TestClass()]
     public class TextSplitHelperNewTests
     {
-        private int LineSpaceY = 14;
-        private int FontHeight = 20;
-        private int FontWidth = 10;
-
-        private PaintContext PaintContext
-        {
-            get
-            {
-                return new PaintContext
-                {
-                    LimitLeft = 0,
-                    LimitRight = 100,
-                    PaintPosX = 100,
-                    PaintPosY = 0,
-                };
-            }
-        }
-
         [TestMethod()]
         public void SplitInvertedAll()
         {
-            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", 0, 15, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", 0, 15, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("1234567890ABCDE", result[0].Text);
@@ -41,7 +23,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitInvertedAll1()
         {
-            var result = TextSplitHelperNew.SplitText("12345 67890 ABCDE", 0, 17, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("12345 67890 ABCDE", 0, 17, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("12345 67890 ABCDE", result[0].Text);
@@ -51,7 +33,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitInvertedNothing()
         {
-            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", -1, 0, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", -1, 0, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("1234567890ABCDE", result[0].Text);
@@ -61,7 +43,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitInvertedNothing1()
         {
-            var result = TextSplitHelperNew.SplitText("12345 67890 ABCDE", 0, 17, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("12345 67890 ABCDE", 0, 17, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("12345 67890 ABCDE", result[0].Text);
@@ -72,7 +54,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitInvertedTest()
         {
-            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", 5, 5, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", 5, 5, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(3, result.Length);
             Assert.AreEqual("12345",result[0].Text);
@@ -87,7 +69,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitInvertedTest1()
         {
-            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", 10, 5, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("1234567890ABCDE", 10, 5, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(2, result.Length);
             Assert.AreEqual("1234567890", result[0].Text);
@@ -99,7 +81,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitEmptyTest()
         {
-            var result = TextSplitHelperNew.SplitText(string.Empty, -1, 0, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText(string.Empty, -1, 0, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(0, result.Length);
         }
@@ -107,7 +89,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitOneCharTest()
         {
-            var result = TextSplitHelperNew.SplitText("A", -1, 0, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("A", -1, 0, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("A", result[0].Text);
@@ -117,7 +99,7 @@ namespace de.springwald.xml.editor.helper.Tests
         [TestMethod()]
         public void SplitOneCharTestInverted()
         {
-            var result = TextSplitHelperNew.SplitText("A", 0, 1, PaintContext, LineSpaceY, FontHeight, FontWidth).ToArray();
+            var result = TextSplitHelperNew.SplitText("A", 0, 1, 1000, 1000).ToArray();
             Assert.IsNotNull(result);
             Assert.AreEqual(1, result.Length);
             Assert.AreEqual("A", result[0].Text);
