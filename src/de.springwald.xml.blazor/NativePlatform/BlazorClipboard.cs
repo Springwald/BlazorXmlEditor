4f285f9a87e384ee122fcbf2dd2be032f4fa49ce
Daniel Springwald <Springwald@users.noreply.github.com>
2020-10-24 22:59:15 +0200

    action toolbar working

--- src/de.springwald.xml.blazor/NativePlatform/BlazorClipboard.cs
+++ src/de.springwald.xml.blazor/NativePlatform/BlazorClipboard.cs
@@ -4,7 +4,7 @@ namespace de.springwald.xml.blazor.NativePlatform
 {
     public class BlazorClipboard : de.springwald.xml.editor.nativeplatform.IClipboard
     {
-        public bool ContainsText => throw new NotImplementedException();
+        public bool ContainsText => false;
 
         public void Clear()
         {
