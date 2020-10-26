ee5ab9633c9e6c3ce9141ae37d04c884626f1b61
Daniel Springwald <Springwald@users.noreply.github.com>
2020-10-27 00:48:03 +0100

    refactoring

--- src/de.springwald.xml.editor/editor/actions/InsertAtCursorPosHelper.cs
+++ src/de.springwald.xml.editor/editor/actions/InsertAtCursorPosHelper.cs
@@ -1,4 +1,12 @@
-﻿using de.springwald.xml.cursor;
+﻿// A platform indepentend tag-view-style graphical xml editor
+// https://github.com/Springwald/BlazorXmlEditor
+//
+// (C) 2020 Daniel Springwald, Bochum Germany
+// Springwald Software  -   www.springwald.de
+// daniel@springwald.de -  +49 234 298 788 46
+// All rights reserved
+// Licensed under MIT License
+
 using de.springwald.xml.rules;
 using System;
 using System.Diagnostics;
