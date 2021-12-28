271785cdb1798efdbbc6e2f0a1b2b4ea26791b09
Daniel Springwald <Springwald@users.noreply.github.com>
2021-12-29 00:01:42 +0100

    Upgraded from .net core 5 to 6

--- src/de.springwald.xml.blazor.demo/Program.cs
+++ src/de.springwald.xml.blazor.demo/Program.cs
@@ -1,27 +1,13 @@
 using CurrieTechnologies.Razor.Clipboard;
+using de.springwald.xml.blazor.demo;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace de.springwald.xml.blazor.demo
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddClipboard();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddClipboard();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
